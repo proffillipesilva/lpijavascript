d8d5a7b46b5fca9c22a931083094760000118eb4
proffillipesilva <81185103+proffillipesilva@users.noreply.github.com>
2023-02-27 22:57:11 -0300

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,8 @@
+const DB = [
+    { email: "joao@mail.com", password: "admin123@#"},
+    { email: "raissa@mail.com", password: "admin123@#"},
+    { email: "piloto@mail.com", password: "admin123@#"},
+    { email: "mariana@mail.com", password: "admin123@#"}
+]
+
+module.exports =  DB 
