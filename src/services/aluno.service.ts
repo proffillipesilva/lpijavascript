411a6be7e489b75c784fd5ee821d7764adc217df
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2023-03-27 22:22:33 -0300

    Finalizando o CRUD

--- src/services/aluno.service.ts
+++ src/services/aluno.service.ts
@@ -1,24 +1,31 @@
 import Aluno from "../models/Aluno";
 import DBMock from "../models/DBMock";
 
-function registraAluno(aluno: Aluno){
+function registraAluno(aluno: Aluno){ // CRIA
     DBMock.tb_alunos.push(aluno);
 }
 
 function editaInfoAluno(alunoEditado: Aluno, index: number){
-    DBMock.tb_alunos.splice(index, 1, alunoEditado);
+    DBMock.tb_alunos.splice(index, 1, alunoEditado); // UPDATE
 }
 
-function consultaAlunos() : Aluno[] {
+function consultaAlunos() : Aluno[] { // READ
     return DBMock.tb_alunos;
 }
 
-function deletaAluno(idx: number) {
+function consultaAlunoEspecifico(idx: number) : Aluno | null { // READ
+    if(idx >= 0 && idx < DBMock.tb_alunos.length)
+        return DBMock.tb_alunos[idx];
+    return null;
+}
 
+function deletaAluno(idx: number) : void {  // DELETE
+    DBMock.tb_alunos.splice(idx, 1);
 }
 
 export default { registraAluno, 
     editaInfoAluno, 
     consultaAlunos, 
-    deletaAluno
+    deletaAluno,
+    consultaAlunoEspecifico
 };
